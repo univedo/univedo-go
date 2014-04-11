b9330c51d0c4df59a41d4e460ad5cd3350b46456
Lucas Clemente <lucas@clemente.io>
2014-04-11 14:22:59 +0200

    test notifications

--- remote_object_test.go
+++ remote_object_test.go
@@ -18,6 +18,15 @@ func (s *testSession) sendMessage(msg []interface{}) error {
 
 func TestRemoteObject(t *testing.T) {
 	Convey("remote objects", t, func() {
+		Convey("send notifications", func() {
+			s := new(testSession)
+			s.onMessage = func() {}
+			ro := createBasicRO(23, s)
+			err := ro.SendNotification("foo", []interface{}{1, "2", 3})
+			So(err, ShouldBeNil)
+			So(s.msg, ShouldResemble, []interface{}{23, 3, "foo", []interface{}{1, "2", 3}})
+		})
+
 		Convey("does rom calls", func() {
 			s := new(testSession)
 			ro := createBasicRO(23, s)
