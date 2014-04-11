44f7ebd59fdb5e0641036d2975ae5bd549558121
Lucas Clemente <lucas@clemente.io>
2014-04-11 23:28:01 +0200

    ping tests

--- session_test.go
+++ session_test.go
@@ -3,9 +3,20 @@ package univedo
 import (
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+	"time"
 )
 
 const URL = "ws://localhost:9000/f8018f09-fb75-4d3d-8e11-44b2dc796130"
+
+func pingTest(v interface{}) {
+	session, err := Dial(URL)
+	So(err, ShouldBeNil)
+	So(session, ShouldNotBeNil)
+	pong, err := session.Ping(v)
+	So(err, ShouldBeNil)
+	So(pong, ShouldResemble, v)
+	session.Close()
+}
 
 func TestSession(t *testing.T) {
 	Convey("session", t, func() {
@@ -17,13 +28,44 @@ func TestSession(t *testing.T) {
 		})
 
 		Convey("pings null", func() {
-			session, err := Dial(URL)
-			So(err, ShouldBeNil)
-			So(session, ShouldNotBeNil)
-			pong, err := session.Ping(nil)
-			So(err, ShouldBeNil)
-			So(pong, ShouldBeNil)
-			session.Close()
+			pingTest(nil)
+		})
+
+		Convey("pings true", func() {
+			pingTest(true)
+		})
+
+		Convey("pings false", func() {
+			pingTest(false)
+		})
+
+		Convey("pings ints", func() {
+			pingTest(uint64(42))
+		})
+
+		Convey("pings negative ints", func() {
+			pingTest(int64(-42))
+		})
+
+		Convey("pings floats", func() {
+			pingTest(1.1)
+		})
+
+		Convey("pings floats", func() {
+			pingTest(1.1)
+		})
+
+		Convey("pings strings", func() {
+			pingTest("foobar")
+		})
+
+		Convey("pings arrays", func() {
+			pingTest([]interface{}{"1", "2"})
+		})
+
+		Convey("pings times", func() {
+			t, _ := time.Parse(time.RFC3339Nano, "2013-03-21T20:04:00.000001Z")
+			pingTest(t)
 		})
 	})
 }
